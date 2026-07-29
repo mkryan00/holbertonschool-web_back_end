f0b8c594c35248a25539191a1c8e486093a30c17
mkryan00 <13073@holbertonstudents.com>
2026-07-29 12:00:21 +1000

    Added task 5. Complex types - list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Task 5 - Sums a list of floats as arguments and returns float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Compute the sum of a list of floats.
+    """
+    return float(sum(input_list))
