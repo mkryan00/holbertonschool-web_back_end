a4a371f2d541cc663a71fa13900af622689bf4a9
mkryan00 <13073@holbertonstudents.com>
2026-07-29 10:12:00 +1000

    Added task 3. Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Task 3 Module - To String.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns string representation of floating point number.
+    """
+    return str(n)
