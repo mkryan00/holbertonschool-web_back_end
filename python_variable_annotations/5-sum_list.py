bf0e3455cd62da1146c7a3b9f5143c39f3fa4c03
mkryan00 <13073@holbertonstudents.com>
2026-07-29 12:04:38 +1000

    Defined List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Task 5 - Sums a list of floats as arguments and returns float.
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Compute the sum of a list of floats.
     """
     return float(sum(input_list))
