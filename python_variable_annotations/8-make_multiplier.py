fdcd0d9ec81215be1dc4f707466b9533e74a93ee
mkryan00 <13073@holbertonstudents.com>
2026-08-06 14:09:09 +1000

    Added task 8. Complex types - functions

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Task 8 - Module for creating a multiplier function."""
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by a multiplier."""
+
+    def multiplier_function(n: float) -> float:
+        return n * multiplier
+    return multiplier_function
