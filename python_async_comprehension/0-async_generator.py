c1dde283ab4547cc04dae6ea62bd24f047cf4349
mkryan00 <13073@holbertonstudents.com>
2026-08-06 16:11:29 +1000

    Added task 0. Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that defines an asynchronous generator."""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Yield 10 random numbers between 0 and 10, one per second."""
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
