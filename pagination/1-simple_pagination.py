c360f89c8672845cf183349d420d2315b1a039bb
mkryan00 <13073@holbertonstudents.com>
2026-08-12 09:46:38 +1000

    Added docstring to get page function

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -45,6 +45,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the correct page of the dataset based on page and page_size."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         data = self.dataset()
