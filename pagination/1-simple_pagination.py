b3e4ae6993a1f025e4874445491dad25e6d29d06
mkryan00 <13073@holbertonstudents.com>
2026-08-12 09:47:34 +1000

    Fixed pycodestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -45,7 +45,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the correct page of the dataset based on page and page_size."""
+        """Return the correct page of the dataset
+        based on page and page_size.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         data = self.dataset()
