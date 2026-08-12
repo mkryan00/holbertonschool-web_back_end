1cefd66f6c3a9b6f9516cb3ca1286e161c7cb083
mkryan00 <13073@holbertonstudents.com>
2026-08-12 12:06:18 +1000

    Added assertions back in

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -57,6 +57,8 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Return dictionary with certain key-value pairs."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
         data = self.get_page(page, page_size)
         dataset = self.dataset()
         total_pages = math.ceil(len(dataset) / page_size)
