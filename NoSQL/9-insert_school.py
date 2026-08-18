19c7cc6a156b1f558e8c0e390c8a26e13697b4d3
mkryan00 <13073@holbertonstudents.com>
2026-08-19 08:09:02 +1000

    Fixed spelling mistake on line 8 - inserted

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -5,4 +5,4 @@
 def insert_school(mongo_collection, **kwargs):
     """"Inserts a document into a collection and returns the ID."""
     result = mongo_collection.insert_one(kwargs)
-    return result.inserterd_id
+    return result.inserted_id
