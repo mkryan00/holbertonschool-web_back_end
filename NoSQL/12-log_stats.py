fdb57da5c6622c725991374a8df6622e960cbfac
mkryan00 <13073@holbertonstudents.com>
2026-08-19 08:30:34 +1000

    Added guard

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,17 +6,18 @@ about Nginx logs stored in MongoDB.
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-db = client.logs
-nginx = db.nginx
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx = db.nginx
 
-total = nginx.count_documents({})
-print(f"{total} logs")
+    total = nginx.count_documents({})
+    print(f"{total} logs")
 
-print("Methods:")
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    count = nginx.count_documents({"method": method})
-    print(f"\t{method}: {count}")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = nginx.count_documents({"method": method})
+        print(f"\t{method}: {count}")
 
-status = nginx.count_documents({"method": "GET", "path": "/status"})
-print(f"{status} status check")
+    status = nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status} status check")
